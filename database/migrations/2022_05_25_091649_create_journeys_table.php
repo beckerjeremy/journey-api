7ea09b57611cbb1fb8b30387c6033090d681f5e6
Jeremy Becker <jeremy@becker.dev>
2022-05-25 11:18:47 +0200

    Added journey migration.

---
+++ database/migrations/2022_05_25_091649_create_journeys_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('journeys', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamp('started_at')->nullable();
+            $table->unsignedBigInteger('status_id');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('status_id')->references('id')->on('status');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('journeys');
+    }
+};
