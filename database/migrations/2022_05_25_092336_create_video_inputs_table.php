892641bc4d14c28b0f04a2ada4c4c397c1cc0ade
Jeremy Becker <jeremy@becker.dev>
2022-05-25 11:25:17 +0200

    Added input migrations.

---
+++ database/migrations/2022_05_25_092336_create_video_inputs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video_inputs', function (Blueprint $table) {
+            $table->id();
+            $table->string('type')->nullable();
+            $table->integer('width')->nullable();
+            $table->integer('height')->nullable();
+            $table->integer('duration')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('video_inputs');
+    }
+};
