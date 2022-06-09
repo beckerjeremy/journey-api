cd022a6d398bc92be767590f5fc4b99e44a84ed3
Jeremy Becker <jeremy@becker.dev>
2022-06-09 08:28:26 +0200

    Fixed error in video resource.

--- app/Http/Resources/VideoInputBasicResource.php
+++ app/Http/Resources/VideoInputBasicResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class VideoInputBasicResoure extends JsonResource
+class VideoInputBasicResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
