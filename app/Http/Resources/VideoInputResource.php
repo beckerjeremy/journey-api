bfe31f2e09f52cd896c8467ec53f461c430087ee
Jeremy Becker <jeremy@becker.dev>
2022-06-15 18:20:21 +0200

    Renamed VideoResource

--- app/Http/Resources/VideoInputResource.php
+++ app/Http/Resources/VideoInputResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use App\Http\Resources\InputBasicResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class VideoInputResoure extends JsonResource
+class VideoInputResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
