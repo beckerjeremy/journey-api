6bab62715a1b492f0d5f8eaa08cae640e95c045e
admin admin <admin@example.com>
2022-08-06 17:54:17 +0000

    New file .space.kts

---
+++ .space.kts
@@ -0,0 +1,16 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Hello World!") {
+    container(image = "composer:2.3") {
+    	shellScript {
+            content =  """
+            		composer install
+                    ./vendor/bin/phpunit
+                """
+        }
+    }
+}
