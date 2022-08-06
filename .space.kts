8fbc6667ecb900b2a530031fec35f77027290ac2
admin admin <admin@example.com>
2022-08-06 19:03:07 +0000

    Updated automation

--- .space.kts
+++ .space.kts
@@ -8,15 +8,18 @@ job("Run Tests") {
     container(image = "composer:2.3") {
     	shellScript {
             content =  """
+            		echo Installing dependencies...
             		composer install
-                    ./vendor/bin/phpunit > /mnt/space/share/tests.txt
+                    
+                    echo Running tests...
+                    ./vendor/bin/phpunit --log-junit ~/phpunit/junit.xml
+                    
+                    echo Uploading artifacts...
+                    SOURCE_PATH=build-logs/log.txt
+                	TARGET_PATH=logs/${'$'}JB_SPACE_EXECUTION_NUMBER/
+                	REPO_URL=https://space.becker.dev/p/journey-api/documents/files
+                	curl -i -H "Authorization: Bearer ${'$'}JB_SPACE_CLIENT_TOKEN" -F file=@"${'$'}SOURCE_PATH" ${'$'}REPO_URL/${'$'}TARGET_PATH
                 """
-        }
-    }
-    
-    docker {
-    	beforeBuildScript {
-            "cp /mnt/space/share docker"
         }
     }
 }
